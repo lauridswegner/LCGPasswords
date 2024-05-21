b9c2f73b21f9dcab20d15dd674e435f3c14a84f0
Laurids Wegner <laurids.wegner@nordakademie.de>
2024-05-21 09:30:53 +0200

    added method to extract random char of a str

--- implementation/lcg.py
+++ implementation/lcg.py
@@ -11,9 +11,13 @@ class LCGPseudoRandomizer:
         self.c: int = c
 
         if seed is None:
+
             self.x0: int = int(os.getpid() + time.time()) * time.monotonic_ns()
+
         else:
+
             self.x0: int = seed
+
         self.x_prev: int = (self.a * self.x0 + self.c) % self.m
 
     def randomint(self, lower_limit: Optional[int] = None, upper_limit: Optional[int] = None) -> int:
@@ -21,14 +25,23 @@ class LCGPseudoRandomizer:
         self.x_prev = (self.a * self.x_prev + self.c) % self.m
 
         if (lower_limit != None and upper_limit != None):
+
             return int((self.x_prev / (self.m -1)) * (upper_limit - lower_limit) + lower_limit)
+        
         else:
+
             return self.x_prev
         
+    def randomchoice(self, input_str: str):
 
-lcg = LCGPseudoRandomizer()
-print(lcg.randomint(1,101))
-print(lcg.randomint(1,101))
-print(lcg.randomint(1,101))
-print(lcg.randomint(1,101))
-print(lcg.randomint(1,101))
+        string_length = len(input_str)
+
+        if string_length > 1:
+            
+            lower_limit: int = 0
+            upper_limit: int = string_length
+
+            return input_str[self.randomint(lower_limit, upper_limit)]
+
+        else:
+            return input_str
